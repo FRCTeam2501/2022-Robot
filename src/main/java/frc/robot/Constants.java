0de9f0b47f41cfe31abbed44a8b94cccbc822417
TylerS1066 <50031864+TylerSeiford@users.noreply.github.com>
2022-01-15 11:06:36 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
 public final class Constants {
-    
+    public static final int LEFT_FRONT_ID = 1;
+    public static final int RIGHT_FRONT_ID = 2;
+    public static final int LEFT_REAR_ID = 3;
+    public static final int RIGHT_REAR_ID = 4;
+
+    public static final double FORWARD_SPEED = 1.0;
+    public static final double ROTATION_SPEED = 0.6;
 }

9d52a61cbcf1ba60d8dfd8cc64bd6f3d137fbea3
TylerS1066 <50031864+TylerSeiford@users.noreply.github.com>
2022-02-01 16:16:58 -0600

    Convert ClimberExtend from a Subsystem to a Sendable

--- src/main/include/subsystems/ClimberExtend.h
+++ src/main/include/subsystems/ClimberExtend.h
@@ -1,11 +1,12 @@
 #pragma once
-#include "frc/motorcontrol/MotorControllerGroup.h"
-#include "frc2/command/SubsystemBase.h"
 #include "rev/CANSparkMax.h"
+#include "wpi/sendable/Sendable.h"
+#include "wpi/sendable/SendableHelper.h"
 #include "units/angle.h"
 
 
-class ClimberExtend : public frc2::SubsystemBase {
+class ClimberExtend : public wpi::Sendable,
+                      public wpi::SendableHelper<ClimberExtend> {
   private:
     // Individual speed controllers
     rev::CANSparkMax *winch;

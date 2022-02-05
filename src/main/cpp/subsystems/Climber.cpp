3062a6ed290a8878750f6fc67d19ae38d103c7bc
TylerS1066 <50031864+TylerSeiford@users.noreply.github.com>
2022-02-05 14:48:56 -0600

    Initial constraints

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -1,4 +1,5 @@
 #include "subsystems/Climber.h"
+#include "units/math.h"
 
 
 Climber::Climber() {
@@ -15,15 +16,115 @@ units::meter_t Climber::GetExtension() {
     return extend->GetExtension();
 }
 
-void Climber::SetExtension(units::meter_t distance) {
-    extend->SetExtension(distance);
+bool Climber::SetExtension(units::meter_t extension) {
+    return Set(extension, GetAngle());
 }
 
 units::degree_t Climber::GetAngle() {
     return rotate->GetAngle();
 }
 
-void Climber::SetAngle(units::degree_t angle) {
-    rotate->SetAngle(angle);
+bool Climber::SetAngle(units::degree_t angle) {
+    return Set(GetExtension(), angle);
 }
 
+bool Climber::Set(units::meter_t extension, units::degree_t angle) {
+    bool result = false;
+
+    // Hard extension limits
+    if(extension > CONSTANTS::CLIMBER::EXTENSION_MAXIMUM) {
+        extension = CONSTANTS::CLIMBER::EXTENSION_MAXIMUM;
+        wpi::outs() << "Climber: constrained extension to: "
+                << extension.to<double>() << "m (hard)\n";
+        result = true;
+    }
+    else if(extension < CONSTANTS::CLIMBER::EXTENSION_MINIMUM) {
+        extension = CONSTANTS::CLIMBER::EXTENSION_MINIMUM;
+        wpi::outs() << "Climber: constrained extension to: "
+                << extension.to<double>() << "m (hard)\n";
+        result = true;
+    }
+
+    // Hard angle limits
+    if(angle > CONSTANTS::CLIMBER::ANGLE_MAXIMUM) {
+        angle = CONSTANTS::CLIMBER::ANGLE_MAXIMUM;
+        wpi::outs() << "Climber: constrained angle to: "
+                << angle.to<double>() << "deg (hard)\n";
+        result = true;
+    }
+    else if(angle < CONSTANTS::CLIMBER::ANGLE_MINIMUM) {
+        angle = CONSTANTS::CLIMBER::ANGLE_MINIMUM;
+        wpi::outs() << "Climber: constrained angle to: "
+                << angle.to<double>() << "deg (hard)\n";
+        result = true;
+    }
+
+    // Handle the battery box constraint
+    if(angle < CONSTANTS::CLIMBER::BATTERY_BOX_ANGLE_MAXIMUM
+            && angle > CONSTANTS::CLIMBER::BATTERY_BOX_ANGLE_MINIMUM) {
+        if(extension < CONSTANTS::CLIMBER::BATTERY_BOX_EXTENSION_MINIMUM) {
+            extension = CONSTANTS::CLIMBER::BATTERY_BOX_EXTENSION_MINIMUM;
+            wpi::outs() << "Climber: constrained extension to: "
+                    << extension.to<double>()
+                    << "m (battery box constraint)\n";
+            result = true;
+        }
+    }
+
+    // Handle the wall constraint
+    units::meter_t minimum;
+    /* minimum = k1 - ((k2 - v1) / v2)
+     * k1: ARM_BACKWARD_LENGTH
+     * k2: FRAME_BACK_OFFSET + FRAME_EXTENSION_MAXIMUM - SAFETY_MARGIN
+     * v1: ARM_OFFSET + ARM_WIDTH * cosine(angle)
+     * v2: sine(angle)
+     */
+    // k2
+    minimum = (CONSTANTS::CLIMBER::FRAME_BACK_OFFSET
+            + CONSTANTS::CLIMBER::FRAME_EXTENSION_MAXIMUM
+            - CONSTANTS::CLIMBER::SAFETY_MARGIN);
+    // minus v1
+    minimum -= (CONSTANTS::CLIMBER::ARM_OFFSET + CONSTANTS::CLIMBER::ARM_WIDTH)
+            * units::math::cos(angle);
+    // divided by v2
+    minimum /= units::math::sin(angle);
+    // k1 minus
+    minimum = CONSTANTS::CLIMBER::ARM_BACKWARD_LENGTH - minimum;
+    if(extension < minimum) {
+        extension = minimum;
+        wpi::outs() << "Climber: constrained extension to: "
+                << extension.to<double>()
+                << "m (wall constraint)\n";
+        result = true;
+    }
+
+    // Handle the ceiling constraint
+    units::meter_t maximum;
+    /* maximum = ((k1 - v1) / v2) - k2
+     * k1: HEIGHT_MAXIMUM - HEIGHT_OFFSET
+     * v1: ARM_OFFSET + HOOK_WIDTH * sine(angle)
+     * v2: cosine(angle)
+     * k2: ARM_FORWARD_LENGTH
+     */
+    // k1
+    maximum = (CONSTANTS::CLIMBER::HEIGHT_MAXIMUM
+            - CONSTANTS::CLIMBER::HEIGHT_OFFSET);
+    // minus v1
+    maximum -= (CONSTANTS::CLIMBER::ARM_OFFSET + CONSTANTS::CLIMBER::HOOK_WIDTH)
+            * units::math::sin(angle);
+    // divided by v2
+    maximum /= units::math::cos(angle);
+    // minus k2
+    maximum -= CONSTANTS::CLIMBER::ARM_FORWARD_LENGTH;
+    if(extension > maximum) {
+        extension = maximum;
+        wpi::outs() << "Climber: constrained extension to: "
+                << extension.to<double>()
+                << "m (ceiling constraint)\n";
+        result = true;
+    }
+
+    extend->SetExtension(extension);
+    rotate->SetAngle(angle);
+    return result;
+}
--- src/main/include/subsystems/Climber.h
+++ src/main/include/subsystems/Climber.h
@@ -4,6 +4,62 @@
 #include "subsystems/climber/ClimberRotate.h"
 
 
+namespace CONSTANTS::CLIMBER {
+    // Hard limits
+    constexpr units::meter_t
+            EXTENSION_MAXIMUM = 3_ft + 1_in,
+            EXTENSION_MINIMUM = 0_in;
+    constexpr units::degree_t
+            ANGLE_MAXIMUM = 90_deg,
+            ANGLE_MINIMUM = 0_deg;
+
+    /*
+     * Battery box constraint
+     *
+     * The arm must be atleast EXTENSION_MINIMUM meters from the bottom
+     * of travel when between ANGLE_MINIMUM and ANGLE_MAXIMUM.
+     */
+    constexpr units::meter_t
+            BATTERY_BOX_EXTENSION_MINIMUM = 8_in;
+    constexpr units::degree_t
+            BATTERY_BOX_ANGLE_MAXIMUM = 15_deg,
+            BATTERY_BOX_ANGLE_MINIMUM = 5_deg;
+
+    /*
+     * Wall constraint
+     *
+     * The climber must be not extend past 16 inches from the end of the frame.
+     */
+    constexpr units::meter_t
+            // Offset from the back of the frame to the rotation point
+            FRAME_BACK_OFFSET = 26.5_in,
+            // Max extension past the frame (G107 & R105)
+            FRAME_EXTENSION_MAXIMUM = 16_in,
+            // Safety margin from the limits
+            SAFETY_MARGIN = 6_in,
+            // Offset of arm from rotation point
+            ARM_OFFSET = 2.5_in,
+            // Width of the arn
+            ARM_WIDTH = 1_in,
+            // Length from the rotation point to the bottom end of the arm
+            ARM_BACKWARD_LENGTH = 35_in;
+
+    /*
+     * Ceiling constraint
+     *
+     * The climber must be not extend past 5 foot 6 inches from the floor.
+     */
+    constexpr units::meter_t
+            // Max height (G106)
+            HEIGHT_MAXIMUM = 5_ft + 6_in,
+            // Offset from the floor to the rotation point
+            HEIGHT_OFFSET = 3_ft + 4_in,
+            // Width of the arm plus hook
+            HOOK_WIDTH = 4_in,
+            // Length from the rotation point to the top end of the arm
+            ARM_FORWARD_LENGTH = 11_in;
+}
+
 class Climber : public frc2::SubsystemBase {
   private:
     ClimberExtend *extend;
@@ -14,8 +70,10 @@ class Climber : public frc2::SubsystemBase {
     ~Climber();
 
     units::meter_t GetExtension();
-    void SetExtension(units::meter_t distance);
+    bool SetExtension(units::meter_t extension);
 
     units::degree_t GetAngle();
-    void SetAngle(units::degree_t angle);
+    bool SetAngle(units::degree_t angle);
+
+    bool Set(units::meter_t extension, units::degree_t angle);
 };
